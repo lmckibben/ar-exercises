c2186fabd89d780fa9c996973ab64c25aaaaa25f
Leland Mckibben <lmckibben28@hotmail.com>
2021-10-12 00:08:38 +0000

    finished exercise_2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+@store2 = Store.find(2)
+@store1.name = "New Westminster"
+
+puts @store1.name
+puts @store2.name
