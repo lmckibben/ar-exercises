6c5d409de13dd79f392610e6624abf1c774d843c
Leland Mckibben <lmckibben28@hotmail.com>
2021-10-12 01:46:48 +0000

    added validations

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, presence: true, length: {minimum: 3}
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validate :apparel
+
+  def apparel 
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel || :womens_apparel, "Must carry men or womens apparel, or both")
+    end
+  end
 end
